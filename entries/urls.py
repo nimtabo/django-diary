86773442be88d48c2d3cf77f091ddb5bf6fe609a
nicholas mtabo <nicholasmtabo@yahoo.com>
2019-06-23 17:24:52 +0300

    creat entry

--- entries/urls.py
+++ entries/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
   path('', views.entry_list, name='entry_list'),
+  path('entry/<int:pk>/', views.entry_detail, name='entry_detail'),
+  path('entry/new/', views.post_new, name='post_new'),
 ]
--- entries/views.py
+++ entries/views.py
@@ -1,10 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 from .models import Entry
 from django.utils import timezone
+from .forms import EntryForm
+from django.shortcuts import redirect
 
 
 def entry_list(request):
   entries = Entry.objects.order_by('-created_date')
   return render(request, 'entries/entry_list.html', {'entries': entries})
+
+def entry_detail(request, pk):
+  entry = get_object_or_404(Entry, pk=pk)
+  return render(request, 'entries/entry_detail.html', {'entry': entry})
+
+def post_new(request):
+  if request.method == "POST":
+    form = EntryForm(request.POST)
+    if form.is_valid():
+      entry = form.save(commit=False)
+      entry.author = request.user
+      entry.save()
+      return redirect('entry_detail', pk=entry.pk)
+  else:
+    form = EntryForm()
+    return render(request, 'entries/post_edit.html', {'form': form})
+
